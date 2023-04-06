e220239db26775e3761670915b659e3d98b04b58
Amber Cronin <amber@amber.vision>
2023-04-06 16:56:30 +0100

    HOTT-2974: Add backend

--- versions.tf
+++ versions.tf
@@ -1,12 +1,19 @@
 terraform {
   required_version = ">= 1.4.0"
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "4.61.0"
     }
   }
-  backend "s3" {}
+
+  backend "s3" {
+    bucket  = "trade-tariff-terraform-state"
+    key     = "common/terraform.tfstate"
+    region  = "eu-west-2"
+    encrypt = true
+  }
 }
 
 provider "aws" {}
